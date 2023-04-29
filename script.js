524ee09b7ec2e1df6710a5be5ec4c5b040284604
Emanuel Nogueira <manunogueira8814@gmail.com>
2023-04-29 11:25:23 -0300

    first Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(num) {
+  document.querySelector(".pepsi").src = num;
+}
+
+function changeColor(num) {
+  const sec = document.querySelector(".sec");
+  sec.style.background = num;
+}
